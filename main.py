34e626e58352bc08c54747742f371de7e6a8454d
juandrzej <programming.yippee729@passinbox.com>
2025-06-09 15:41:28 +0200

    CLI input works

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,12 +7,16 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-)
+try:
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents=sys.argv[1]
+    )
+except IndexError as ie:
+    print(f"IndexError: {ie}")
+    sys.exit(1)
+
 print(f"""
-Response: {response.text}
-Prompt tokens: {response.usage_metadata.prompt_token_count}
-Response tokens: {response.usage_metadata.candidates_token_count}
+    Response: {response.text}
+    Prompt tokens: {response.usage_metadata.prompt_token_count}
+    Response tokens: {response.usage_metadata.candidates_token_count}
 """)
