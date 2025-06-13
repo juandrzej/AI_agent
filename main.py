01b01096ad57202e87635355ee3183312703c349
juandrzej <programming.yippee729@passinbox.com>
2025-06-13 15:34:14 +0200

    agent works, project needs refactoring tho

--- main.py
+++ main.py
@@ -28,29 +28,39 @@ verbose: bool = True if "--verbose" in sys.argv else False
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
+iteration: int = 0
 
-try:
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions], system_instruction=system_prompt
-        ),
-    )
-except IndexError as ie:
-    print(f"IndexError: {ie}")
-    sys.exit(1)
+while iteration < 20:
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions], system_instruction=system_prompt
+            ),
+        )
+    except IndexError as ie:
+        print(f"IndexError: {ie}")
+        sys.exit(1)
 
+    for candidate in response.candidates:
+        messages.append(candidate.content)
 
-if response.function_calls and len(response.function_calls) > 0:
-    for function_call_part in response.function_calls:
-        function_call_result: types.Content = call_function(function_call_part, verbose)
-        if not function_call_result.parts[0].function_response.response:
-            raise Exception("Invalid function response.")
-        if verbose:
-            print(f"-> {function_call_result.parts[0].function_response.response}")
-else:
-    print(f"Response: {response.text}")
+    if response.function_calls and len(response.function_calls) > 0:
+        for function_call_part in response.function_calls:
+            function_call_result: types.Content = call_function(
+                function_call_part, verbose
+            )
+            if not function_call_result.parts[0].function_response.response:
+                raise Exception("Invalid function response.")
+            if verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+            messages.append(function_call_result)
+    else:
+        print(f"Response: {response.text}")
+        break
+
+    iteration += 1
 
 if verbose:
     print(f"User prompt: {user_prompt}")
